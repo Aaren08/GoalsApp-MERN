b90162bf51691da2decfdf4f32c056859e7b4d97
Aaren08 <schneiderneil392@gmail.com>
2025-06-27 17:08:17 +0500

    Fetch Goals from server & displayed them on DOM

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,17 +1,37 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import GoalForm from "../components/goalForm";
+import { getGoals, reset } from "../features/goals/goalSlice";
+import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
+import Spinner from "../components/Spinner";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
     if (!user) {
       navigate("/login");
     }
-  }, [user, navigate]);
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, isError, message, dispatch, navigate]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -21,6 +41,18 @@ const Dashboard = () => {
       </section>
 
       <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal}></GoalItem>
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   );
 };
