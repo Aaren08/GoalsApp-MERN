06cb86f27d994aec3aecbdd6947f8526a8a47e5a
Aaren08 <schneiderneil392@gmail.com>
2025-06-27 15:44:19 +0500

    Restrict the dashboard

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -44,17 +44,15 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for User
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found!");
   }
 
   // Make sure the logged in user matches the goal user
 
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -77,17 +75,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal deletion failed: Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for User
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found!");
   }
 
   // Make sure the logged in user matches the goal user
 
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
