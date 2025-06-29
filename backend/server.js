87f17e4a44b155eef5090d8177203abf2938ac09
Aaren08 <schneiderneil392@gmail.com>
2025-06-29 20:09:07 +0500

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { goalRouter } from "./routes/goalRoutes.js";
 import { userRouter } from "./routes/userRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 import { connectDB } from "./config/db.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -15,6 +17,25 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded bodies
 app.use("/api/goals", goalRouter);
 app.use("/api/users", userRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    ); // Set the path to your index.html file
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Please set to production");
+  });
+}
+
 app.use(errorHandler); // Custom error handling middleware
 
 app.listen(PORT, () => {
